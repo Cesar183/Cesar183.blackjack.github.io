08dfc32939af6db1bbbf5477018e15993fe4ff24
Cesar Martinez <cesar_a_mar_@hotmail.com>
2025-04-14 16:42:18 -0500

    Boton pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,12 @@ let deck = [];
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "J", "Q", "K"];
 
+let puntosJugador = 0;
+let puntosComputadora = 0;
+const btnPedir = document.querySelector("#btnPedir");
+const puntosHtml = document.querySelectorAll("small");
+const divCartasJugador = document.querySelector("#jugador-cartas");
+
 const crearDeck = () => {
     for(let i = 2; i<=10; i++){
         for(let tipo of tipos){
@@ -32,8 +38,36 @@ const pedirCarta = () => {
         throw "No hay cartas en el deck";
     }
     const carta = deck.pop();
-    console.log(deck);
-    console.log(carta);
     return carta;
 }
 
+//Pedir carta
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length -1);
+    return (isNaN(valor))
+           ? (valor === "A") ? 11 : 10
+           : valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+
+//Evento pedir carta
+btnPedir.addEventListener("click", () => {
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHtml[0].innerText = puntosJugador;
+
+    //Crear carta
+    const imgCarta = document.createElement("img");
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add("carta");
+    divCartasJugador.append(imgCarta);
+
+    if(puntosJugador > 21){
+        console.log("Lo siento mucho, perdiste!");
+        btnPedir.disabled = true;
+    } else if (puntosJugador === 21){
+        console.log("21, genial!");
+        btnPedir.disabled = true;
+    }
+});
